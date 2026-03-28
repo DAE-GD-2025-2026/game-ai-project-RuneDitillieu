193d618a897f88b07cdbb786edc2b6662c257650
Rune Ditillieu <rune.ditillieu@student.howest.be>
2026-03-28 21:29:08 +0100

    added A-star pathfinding

--- Source/GameAIProg/GraphTheory/Algorithms/AStar.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/AStar.cpp
@@ -1,4 +1,5 @@
 ﻿#include "AStar.h"
+#include "String/FormatStringSan.h"
 
 using namespace GameAI;
 
@@ -11,6 +12,100 @@ AStar::AStar(Graph* const pGraph, HeuristicFunctions::Heuristic hFunction)
 std::vector<Node*>AStar::FindPath(Node* const pStartNode, Node* const pGoalNode)
 {
 	std::vector<Node*> path{};
+	
+	std::vector<NodeRecord> openList{ }; // nodes to be checked
+	std::vector<NodeRecord> closedList{ }; // nodes already checked
+	NodeRecord currentNodeRecord{ }; // to be evaluated node
+	
+	currentNodeRecord.pNode = pStartNode;
+	openList.push_back(currentNodeRecord);
+	
+	// start of the algorithm
+	while (!openList.empty())
+	{
+		// record with smallest f-cost
+		currentNodeRecord = *std::min_element(openList.begin(), openList.end(), 
+			[](NodeRecord first, NodeRecord smallest){ return first.estimatedTotalCost < smallest.estimatedTotalCost; });
+		
+		// if curRecord is the destNode, exit loop
+		if (currentNodeRecord.pNode == pGoalNode)
+			break;
+		else
+		{
+			std::vector<Connection*> connections{ pGraph->FindConnectionsFrom(currentNodeRecord.pNode->GetId()) };
+			// for every connection
+			for (Connection* conn : connections)
+			{
+				bool skipToNextNeighbor{ false };
+				
+				// calc G-cost to connected node
+				Node* pNextNode{ pGraph->GetNode(conn->GetToId()).get() };
+				float GCost{ currentNodeRecord.costSoFar + GetHeuristicCost(currentNodeRecord.pNode, pNextNode)};
+				
+				// check if Node is in closed list
+				for (NodeRecord NodeRec : closedList)
+				{
+					if (NodeRec.pNode == pNextNode)
+					{
+						// if the already recorded G-cost is smaller than the current G-cost
+						if (NodeRec.costSoFar <= GCost)
+						{
+							skipToNextNeighbor = true;
+						}
+						else
+						{
+							closedList.erase(std::remove(closedList.begin(), closedList.end(), NodeRec));
+							break;
+						}
+					}
+				}
+				
+				for (NodeRecord NodeRec : openList)
+				{
+					if (NodeRec.pNode == pNextNode)
+					{
+						// if the already recorded G-cost is smaller than the current G-cost
+						if (NodeRec.costSoFar <= GCost)
+						{
+							skipToNextNeighbor = true;
+						}
+						else
+						{
+							openList.erase(std::remove(openList.begin(), openList.end(), NodeRec));
+							break;
+						}
+					}
+				}
+				
+				if (!skipToNextNeighbor)
+				{
+					openList.emplace_back(NodeRecord{ pNextNode, conn, 
+						GCost - GetHeuristicCost(currentNodeRecord.pNode, pNextNode), GCost });
+				}
+			}
+			
+			auto it{ std::remove_if(openList.begin(), openList.end(), 
+				[currentNodeRecord](NodeRecord nr){return nr.pNode == currentNodeRecord.pNode;})};
+			openList.erase(it, openList.end());
+			closedList.push_back(currentNodeRecord);
+		}
+	}
+
+	while (currentNodeRecord.pNode != pStartNode)
+	{
+		path.push_back(currentNodeRecord.pNode);
+		for (NodeRecord NodeRec : closedList)
+		{
+			if (NodeRec.pNode == pGraph->GetNode(currentNodeRecord.pConnection->GetFromId()).get())
+			{
+				currentNodeRecord = NodeRec;
+				break;
+			}
+		}
+	}
+	path.push_back(pStartNode);
+	std::reverse(path.begin(), path.end());
+	
 	return path;
 }
 
