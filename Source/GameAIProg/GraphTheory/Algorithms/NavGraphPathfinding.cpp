07a71d5193cb3c5d0bde3564a874f26a83b8abe8
Rune Ditillieu <rune.ditillieu@student.howest.be>
2026-04-04 16:56:47 +0200

    implemented first iteration of NavMeshPathfinding::FindPath()

--- Source/GameAIProg/GraphTheory/Algorithms/NavGraphPathfinding.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/NavGraphPathfinding.cpp
@@ -15,17 +15,63 @@ std::vector<FVector2D> NavMeshPathfinding::FindPath(const FVector2D& startPos, c
 	std::vector<FVector2D> finalPath{};
 
 	//Get the start and endTriangle
+	FVector2D start{};
+	const TriPolygon::Triangle* startTriangle{ pNavGraph->GetNavPolygon()->GetClosestTriangleToPosition(startPos, start) };
+	FVector2D end{};
+	const TriPolygon::Triangle* endTriangle{ pNavGraph->GetNavPolygon()->GetClosestTriangleToPosition(endPos, end) };
 
+	if (startTriangle == nullptr || endTriangle == nullptr)
+	{
+		return finalPath;
+	}
+	else if (startTriangle == endTriangle)
+	{
+		finalPath.emplace_back(start);
+		return finalPath;
+	}
+	
 	//We have valid start/end triangles and they are not the same
 	//=> Start looking for a path
 	//Copy the graph
+	
+	std::shared_ptr<NavGraph> pGraph{ pNavGraph->Clone() };
 
-	//Create Extra node for the Start Node (Agent's position
+	//Create Extra node for the Start Node (Agent's position)
 
+	int startNodeId{ pGraph->AddNode(std::make_unique<NavGraphNode>(startPos, -1)) };
+	for (auto& edge : startTriangle->GetEdges())
+	{
+		std::optional<int> edgeId{ pNavGraph->GetNavPolygon()->FindEdgeIndex(edge) };
+		int nodeId{ pGraph->GetNodeIdFromEdgeIndex(edgeId.value()) };
+		if (nodeId > -1)
+		{
+			pGraph->AddConnection(startNodeId, nodeId);
+		}
+		
+	}
+	
 	//Create extra node for the endNode
 
+	int endNodeId{ pGraph->AddNode(std::make_unique<NavGraphNode>(endPos, -1)) };
+	for (auto& edge : endTriangle->GetEdges())
+	{
+		std::optional<int> edgeId{pNavGraph->GetNavPolygon()->FindEdgeIndex(edge).value() };
+		int nodeId{ pGraph->GetNodeIdFromEdgeIndex(edgeId.value()) };
+		if (nodeId > -1)
+		{
+			pGraph->AddConnection(nodeId, endNodeId);
+		}
+	}
+	
 	//Run A star on new graph
 
+	AStar astar{ pGraph.get(), HeuristicFunctions::Chebyshev };
+	for (auto* node : astar.FindPath(pGraph->GetNode(startNodeId).get(), pGraph->GetNode(endNodeId).get()))
+	{
+		finalPath.emplace_back(node->GetPosition());
+		debugNodePositions.emplace_back(node->GetPosition());
+	}
+	
 	//Debug Visualisation
 
 	// Extra: Run optimiser on new graph (First check if everything works without SSFA!)
